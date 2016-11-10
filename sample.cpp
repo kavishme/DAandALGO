04f77b6c06af280c62703402d09a0d0f7c4bab10
kavish jain <kavishme@gmail.com>
2016-11-09 18:47:45 -0800

    balanced forest and square ten tree

---
+++ sample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	char a = '9';
+	for (int i = 0; i < 15; ++i)
+	{
+		cout<<a<<endl;
+		--a;
+	}
+	return 0;
+}
--- square10tree2.cpp
+++ square10tree2.cpp
@@ -1,10 +1,24 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+#include <cstring>
+
 using namespace std;
 
 const int N = 1000 * 1000 + 5;
 char L[N], R[N], M[N];
 int n;
 
+void printarr(char* arr){
+    for (int i = 0; i < n; ++i)
+    {
+        cout<<arr[i];
+    }
+    cout<<endl;
+}
 vector < pair<int,string> > split_prefix(char *t) {
     int len = 1;
     int lev = 0;
@@ -31,13 +45,23 @@ vector < pair<int,string> > split_prefix(char *t) {
 }
 
 int main() {
+    //read values from input and take their lenghts
     scanf("%s%s", L, R);
     n = strlen(R);
     int m = strlen(L);
+    cout<<endl<<"Initial Values:"<<endl;
+    printarr(R);
+    printarr(L);
+
+    //allign values in R and L and set all values in M to '0'
     for (int i = 0; i < n; ++i) {
         L[n - i - 1] = (m - i - 1 < 0 ? '0' : L[m - i - 1]);
         M[i] = '0';
     }
+    // cout<<endl<<"After Allignment 1:"<<endl;
+    // printarr(R);
+    // printarr(L);
+
     for (int i = n - 1; i >= 0; --i) {
         if (L[i] != '0') {
             L[i]--;
@@ -45,32 +69,48 @@ int main() {
         }
         L[i] = '9';
     }
+    cout<<endl<<"After allignment reducing L by 1:"<<endl;
+    printarr(R);
+    printarr(L);
 
+    //position of first character that doesnt match
     int lcp = 0;
     while (lcp < n && L[lcp] == R[lcp]) 
         ++lcp;
+    //cout<<endl<<"lcp: "<<lcp<<endl;
 
     vector < pair<int, string> > res, tmp;
     int rg = n - 1;
     int len = 1;
     int lev = 0;
+
+    //find R` and M
     while (rg >= 0) {
         int lf = max(0, rg - len + 1);
 
+        // cout<<endl<<"in loop:"<<endl;
+        // cout<<"lev: "<<lev<<endl<<"lf: "<<lf<<endl<<"rg: "<<rg<<endl<<"len: "<<len<<endl;
+        
         if (lf <= lcp) break;
-
-        int fnz = -1;
         for (int i = lf; i <= rg; ++i) {
             M[i] = R[i];
             R[i] = '0';
         }
 
+        // printarr(R);
+        // printarr(M);
+        
         rg = lf - 1;
         len += len;
         if (lev == 0) len = 1;
         lev++;
     }
 
+    cout<<endl<<"end loop:"<<endl;
+    printarr(R);
+    printarr(M);
+
+    //calculate R` - L
     int rem = 0;
     for (int i = n - 1; i >= 0; --i) {
         R[i] += rem - L[i];
@@ -82,13 +122,18 @@ int main() {
         R[i] += '0';
     }
 
+    cout<<endl<<"R` - L :"<<endl;
+    printarr(R);
+    cout<<endl;
+
     res = split_prefix(R);
-    reverse(res.begin(), res.end());The last thing left is to 
+    reverse(res.begin(), res.end());//The last thing left is to 
+
     tmp = split_prefix(M);
     for (int i = 0; i < tmp.size(); ++i)
         res.push_back(tmp[i]);
 
-    printf("%d\n", res.size());
+    printf("%ld\n", res.size());
     for (int i = 0; i < res.size(); ++i)
         printf("%d %s\n", res[i].first, res[i].second.c_str());
     return 0;
